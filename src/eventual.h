8e34d79a6ca2320a39278a2b3bb2d8b3255a946a
wjpjw <recvfromsockaddr@gmail.com>
2018-10-30 21:43:43 +0800

    w

---
+++ src/eventual.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "internal.h"
+
+/*
+    some notes on highermq's async programming framework design
+        
+        rejected; resolved; sleep; stop
+
+
+*/
+
+
+namespace hmq
+{
+
+class eventual
+{
+public:
+
+private:
+
+
+
+
+};
+
+
+
+}
